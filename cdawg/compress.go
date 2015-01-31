4776ee387bc0be8f80c589be50b7ccf822bac39a
Niraj Thapaliya <niraj.thapaliya@gmail.com>
2015-01-31 15:13:27 -0500

    made CDawg generation deterministic

--- cdawg/compress.go
+++ cdawg/compress.go
@@ -4,43 +4,55 @@ import "github.com/nthapaliya/wordgraph/dawg"
 
 // Compress takes the pointer-to-node based Dawg structure and converts it
 // into an int matrix CDawg
+//
 func Compress(dg *dawg.Dawg) CDawg {
-	matrix := make([][]int, len(dg.Register())+1)
+
+	indices := getIndices(dg)
+	reg := populateReg(indices)
+	matrix := make([][]int, len(indices))
+
 	for i := range matrix {
 		matrix[i] = make([]int, 26)
-	}
-	r := make(map[string]int)
 
-	indices := make([]*dawg.State, len(dg.Register())+1)
-	indices[0] = dg.Root()
-
-	index := 1
-	for k, v := range dg.Register() {
-		_, ok := r[k]
-		if !ok {
-			r[k] = index
-			indices[index] = v
-			index++
-		}
-	}
-	// all set up
-
-	for i, st := range dg.Root().Children() {
-		matrix[0][i] = r[st.Hash()] << 1
-		if st.Final() {
-			matrix[0][i]++
-		}
-	}
-
-	for i := 1; i < len(matrix); i++ {
 		for j, c := range indices[i].Children() {
 			if c != nil {
-				matrix[i][j] = r[c.Hash()] << 1
 				if c.Final() {
-					matrix[i][j]++
+					matrix[i][j] = 1
 				}
+				matrix[i][j] += reg[c.Hash()] << 1
 			}
 		}
 	}
 	return matrix
+}
+
+func getIndices(dg *dawg.Dawg) []*dawg.State {
+	indices := []*dawg.State{}
+	encountered := make(map[string]bool)
+	q := newQueue(100)
+	q.push(dg.Root())
+
+	for q.count != 0 {
+		st := q.pop()
+		if !encountered[st.Hash()] {
+			encountered[st.Hash()] = true
+			indices = append(indices, st)
+
+			for _, c := range st.Children() {
+				if c != nil {
+					q.push(c)
+				}
+			}
+		}
+
+	}
+	return indices
+}
+
+func populateReg(indices []*dawg.State) map[string]int {
+	reg := make(map[string]int)
+	for i, st := range indices {
+		reg[st.Hash()] = i
+	}
+	return reg
 }
